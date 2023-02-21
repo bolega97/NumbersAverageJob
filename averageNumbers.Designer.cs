37835a38b81d9604b8b2bc2ee4fe525c4c431443
Bolega Ádám Balázs <h982094@stud.u-szeged.hu>
2023-02-21 13:14:42 +0100

    Add project files.

---
+++ averageNumbers.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace ArminTradeJob
+{
+    partial class averageNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numTextBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.averageBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numTextBox
+            // 
+            this.numTextBox.Location = new System.Drawing.Point(343, 44);
+            this.numTextBox.Name = "numTextBox";
+            this.numTextBox.Size = new System.Drawing.Size(183, 22);
+            this.numTextBox.TabIndex = 0;
+            // 
+            // addBtn
+            // 
+            this.addBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.addBtn.FlatAppearance.BorderSize = 3;
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addBtn.Location = new System.Drawing.Point(385, 72);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(104, 32);
+            this.addBtn.TabIndex = 1;
+            this.addBtn.Text = "Hozzáad";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Location = new System.Drawing.Point(328, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adjon a listához egy számot";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 290);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // averageBtn
+            // 
+            this.averageBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.averageBtn.Location = new System.Drawing.Point(63, 296);
+            this.averageBtn.Name = "averageBtn";
+            this.averageBtn.Size = new System.Drawing.Size(96, 38);
+            this.averageBtn.TabIndex = 5;
+            this.averageBtn.Text = "Átlag";
+            this.averageBtn.UseVisualStyleBackColor = true;
+            this.averageBtn.Click += new System.EventHandler(this.averageBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(63, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Törlés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // averageNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.averageBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.numTextBox);
+            this.Name = "averageNumbers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AverageNumbers";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numTextBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button averageBtn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ averageNumbers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArminTradeJob
+{
+    public partial class averageNumbers : Form
+    {
+        List<int> list = new List<int>();
+        DataTable dt = new DataTable();
+        BindingSource bs = new BindingSource();
+
+       
+        public averageNumbers()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+          
+
+            if (numTextBox.Text.Length == 0) {
+                MessageBox.Show("Nem írt be számot!", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (numTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Nem írt be számot!", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int parseInt;
+            if (!int.TryParse(numTextBox.Text, out parseInt))
+            {
+                MessageBox.Show("Nem számot írt be!", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int input = Convert.ToInt32(numTextBox.Text);
+                list.Add(input);
+            this.dataGridView1.DataSource = list.Select(k => new { Value = k }).ToList();
+           
+
+        }
+
+        private void averageBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (list.Count<=1 )
+            {
+                MessageBox.Show("Kevés a bevitt szám!", "Hiba!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int darab = list.Count;
+            double average = list.Average();
+            MessageBox.Show("Az átlag:" + average.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            
+            bs.DataSource = dt;
+            dataGridView1.DataSource = bs;
+
+
+        }
+    }
+}
